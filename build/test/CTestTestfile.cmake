84fed2927150268bffec5e8385ee6540391fbaec
Alimuhammad <noutkodera@gmail.com>
2025-09-22 19:22:27 +0300

    решил проблему с билдом

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/noutk/OneDrive/Рабочий стол/учеба/c_graphics/1hw/c_graphics_task1/test
-# Build directory: /mnt/c/Users/noutk/OneDrive/Рабочий стол/учеба/c_graphics/1hw/c_graphics_task1/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[test_edu_math]=] "/mnt/c/Users/noutk/OneDrive/Рабочий стол/учеба/c_graphics/1hw/c_graphics_task1/build/test/test_edu_math")
-set_tests_properties([=[test_edu_math]=] PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/noutk/OneDrive/Рабочий стол/учеба/c_graphics/1hw/c_graphics_task1/test/CMakeLists.txt;7;add_test;/mnt/c/Users/noutk/OneDrive/Рабочий стол/учеба/c_graphics/1hw/c_graphics_task1/test/CMakeLists.txt;0;")
